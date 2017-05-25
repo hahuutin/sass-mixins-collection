e55f04aed2692dd6da34e1321ac273d7f4c6fe2c
Ha Huu Tin <hahuutin1992@gmail.com>
2017-05-26 00:21:31 +0700

    change to eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var eslint = require('gulp-eslint');
 
 gulp.task('connect', function() {
   connect.server({
@@ -17,8 +18,9 @@ gulp.task('file', function () {
 
 gulp.task('linter', function () {
   return gulp.src('./scripts/scripts.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', function () {
